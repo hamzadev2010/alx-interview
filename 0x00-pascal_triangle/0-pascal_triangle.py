7ce3b855a668745a893f26f8605fd27b61e1b2b5
hamza chankour <hamzachanik@outlook.com>
2024-04-11 13:55:59 -0700

    task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,7 +2,7 @@ def pascal_triangle(n):
     """
     rturns a list of lists of integers representing the Pascal’s triangle of n
     """
-    if n <= 0:
+     if n <= 0:
         return []
 
     triangle = []
@@ -10,8 +10,10 @@ def pascal_triangle(n):
         row = []
         for col_num in range(row_num + 1):
             if col_num == 0 or col_num == row_num:
+                # First and last elements of each row are always 1
                 row.append(1)
             else:
+                # Calculate the value based on the values from the previous row
                 prev_row = triangle[row_num - 1]
                 value = prev_row[col_num - 1] + prev_row[col_num]
                 row.append(value)
