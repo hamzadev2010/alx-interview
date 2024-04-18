28b28d73c755b5f0f3ee5211444afbb37a45ecb6
hamza chankour <hamzachanik@outlook.com>
2024-04-18 08:17:36 -0700

    task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''LockBoxes'''
+
+def canUnlockAll(boxes):
+    """
+    Checks if all lockboxes can be opened.
+
+    Parameters:
+        boxes (list of lists): Boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+
+    opened_boxes = set()
+
+    def open_box(box_index):
+        if box_index in opened_boxes:
+            return
+        opened_boxes.add(box_index)
+        for key in boxes[box_index]:
+            open_box(key)
+
+    open_box(0)
+    
+    return len(opened_boxes) == len(boxes)
