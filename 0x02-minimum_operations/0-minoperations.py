8bc5996ba36f529b75c1ecc102ec3190b2155f9d
hamza chankour <hamzachanik@outlook.com>
2024-04-25 04:48:30 -0700

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,21 +5,21 @@ of operations needed to result in exactly n H characters in the file."""
 def minOperations(n):
     if n < 2:
         return 0
-    
+
     pr = 0
-    
+
     while n % 2 == 0:
         pr += 2
         n //= 2
-    
+
     dv = 3
     while dv * dv <= n:
         while n % dv == 0:
             pr += dv
             n //= dv
         dv += 2
-        
+
     if n > 1:
         pr += n
-    
+
     return pr
