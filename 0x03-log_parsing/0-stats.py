0f1cf8a07e303a7a3de96a86c93f6c78ea3decfb
hamza chankour <hamzachanik@outlook.com>
2024-05-02 06:42:18 -0700

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,52 @@
 #!/usr/bin/python3
 """ script that reads stdin line by line and computes metrics"""
+
 import sys
 import re
 
-def display_statistics(status_stats: dict, total_size: int) -> None:
-    print("Total File Size: {:d}".format(total_size))
-    for status_code, count in sorted(status_stats.items()):
+def is_log_format_valid(log_string):
+    regex_pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d+'
+    return re.match(regex_pattern, log_string)
+
+def print_statistics(sorted_keys_dict, total_size):
+    """
+    function to print
+    the values to standard output
+    """
+    print("Total File Size:", total_size)
+    for status_code, count in sorted_keys_dict.items():
         if count:
             print("{}: {}".format(status_code, count))
 
-def parse_input_lines(input_lines: iter) -> None:
-    total_size, line_count = 0, 0
-    status_stats = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-
-    try:
-        line = input_lines.readline()
-        while line:
-            line_count += 1
-            match = re.match(r'.* (.{3}) (\d+)$', line.strip())
-            if match:
-                status_code, size = match.groups()
-                if status_code in status_stats:
-                    status_stats[status_code] += 1
-                total_size += int(size)
-            if line_count % 10 == 0:
-                display_statistics(status_stats, total_size)
-            line = input_lines.readline()
-        display_statistics(status_stats, total_size)
-    except KeyboardInterrupt:
-        display_statistics(status_stats, total_size)
-        raise
+def read_and_analyze_logs():
+    while True:
+        try:
+            status_codes = {
+                "200": 0,
+                "301": 0,
+                "400": 0,
+                "401": 0,
+                "403": 0,
+                "404": 0,
+                "405": 0,
+                "500": 0
+            }
+            total_size = 0
+            for line_count in range(11):
+                log_info = sys.stdin.readline().strip()
+                if is_log_format_valid(log_info):
+                    log_info_list = log_info.split(" ")
+                    file_size = log_info_list[-1]
+                    status_code = log_info_list[-2]
+                    status_codes[status_code] = status_codes.get(status_code, 0) + 1
+                    total_size += int(file_size)
+                    sorted_keys_dict = {code: status_codes[code] for code in sorted(status_codes) if status_codes[code] != 0}
+                if line_count % 10 == 0:
+                    print_statistics(sorted_keys_dict, total_size)
+            print_statistics(sorted_keys_dict, total_size)
+        except KeyboardInterrupt:
+            print_statistics(sorted_keys_dict, total_size)
+            raise
 
 if __name__ == '__main__':
-    parse_input_lines(sys.stdin)
+    read_and_analyze_logs()
