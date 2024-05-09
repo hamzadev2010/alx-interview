fe015501096e9a7e178d133ead0ac4f54033497a
hamza chankour <hamzachanik@outlook.com>
2024-05-08 18:04:38 -0700

    task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""script of method that determines if a
+ given data set represents a valid UTF-8 encoding."""
+
+
 def validUTF8(data):
     numofbyte = 0
     # loop to return the number of bytes
