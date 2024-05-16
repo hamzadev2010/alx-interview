8a5babda1019695a85601a27a16f51433d427463
hamza chankour <hamzachanik@outlook.com>
2024-05-16 08:39:44 -0700

    task

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+'''the queens Solver'''
+
+import sys
+
+
+def is_safe(board, row, col, n):
+    # Check if there's a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    
+    # Check upper-left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    # Check upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
+            return False
+    
+    return True
+
+
+def solve_nqueens_util(board, row, n, solutions):
+    if row == n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return True
+    
+    res = False
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            res = solve_nqueens_util(board, row + 1, n, solutions) or res
+            board[row][col] = 0
+    
+    return res
+
+
+def solve_nqueens(n):
+    if n < 4:
+        print('N must be at least 4')
+        return []
+    
+    board = [[0] * n for _ in range(n)]
+    solutions = []
+    
+    if not solve_nqueens_util(board, 0, n, solutions):
+        print(f"No solution found for {n} queens.")
+    
+    return solutions
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    solutions = solve_nqueens(n)
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
