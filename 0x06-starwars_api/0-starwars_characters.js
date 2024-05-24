c102cbf0908166ec669f769f3a3e0eb2d024ca45
hamzadev2010 <hamzachanik@outlook.com>
2024-05-23 19:22:32 -0700

    task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/* script that prints all characters of a Star Wars movie */
+
+const request = require('request');
+const fId = process.argv[2].toString();
+const starwarsurl = `https://swapi-api.alx-tools.com/api/films/${fId}`;
+
+request(starwarsurl, function (error, _, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const objects = JSON.parse(body);
+  const characters = objects.characters;
+  printCharacters(characters);
+});
+
+function printCharacters(characters, index = 0) {
+  request(characters[index], function (error, _, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(JSON.parse(body).name);
+    if (++index < characters.length) {
+      printCharacters(characters, index);
+    }
+  });
+ }
